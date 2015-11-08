d93ee25621a3a83a42159b8ba6ac17c660a42311
tima55510 <tima55510@gmail.com>
2015-11-08 14:42:40 +0300

    Добавлены коллизии

--- SFML5/main.cpp
+++ SFML5/main.cpp
@@ -7,57 +7,30 @@
 using namespace std;
 
 
-/*void movement_hero(Sprite &herosprite, float time, float &CurrentFrame){
-	if (Keyboard::isKeyPressed(Keyboard::Left)) {
-		CurrentFrame += 0.005*time;
-		if (CurrentFrame > 2) (CurrentFrame -= 2);
-		herosprite.setTextureRect(IntRect(201, 67 * int(CurrentFrame), 65, 65));
-		herosprite.move(-0.1*time, 0);
-	} 
-	else if (Keyboard::isKeyPressed(Keyboard::Right)){
-		CurrentFrame += 0.005*time;
-		if (CurrentFrame > 2) (CurrentFrame -= 2);
-		herosprite.setTextureRect(IntRect(0, 67 * int(CurrentFrame), 65, 65));
-		herosprite.move(0.1 * time, 0); 
-	} 
-	else if (Keyboard::isKeyPressed(Keyboard::Up)) {
-		CurrentFrame += 0.005*time;
-		if (CurrentFrame > 2) (CurrentFrame -= 2);
-		herosprite.setTextureRect(IntRect(134, 67 * int(CurrentFrame), 65, 65));
-		herosprite.move(0, -0.1 * time);
-	} 
-	else if (Keyboard::isKeyPressed(Keyboard::Down)){ 
-		CurrentFrame += 0.005*time;
-		if (CurrentFrame > 2) (CurrentFrame -= 2);
-		herosprite.setTextureRect(IntRect(67, 67 * int(CurrentFrame), 65, 65));
-		herosprite.move(0, 0.1 * time); 
-	} 
-}*/
 
-/*void drawing(RenderWindow &window, Sprite &herosprite, Sprite &tex) {
-	window.clear();
-	window.draw(tex);
-	window.draw(herosprite);
-	window.display();
-}*/
 
 class Player {
 private:float x, y = 0;
 public:
 	float w, h, dx, dy, speed = 0;
-	int dir = 0;
+	int dir;
+	int playerScore, health;
+	bool life;
 	String File;
 	Image image;
 	Texture texture;
 	Sprite sprite;
 	Player(String F, float X, float Y, float W, float H) {
+		dir = 0; speed = 0; playerScore = 0; health = 100;
+		life = true;
 		File = F;
 		w = W; h = H;
 		image.loadFromFile("images/" + File);
+		image.createMaskFromColor(Color(0, 0, 0));
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
 		x = X, y = Y;
-		sprite.setTextureRect(IntRect(99, 99, w, h));
+		sprite.setTextureRect(IntRect(0, 0, w, h));
 	}
 
 	void update(float time) {
@@ -71,6 +44,37 @@ public:
 		y += dy*time;
 		speed = 0;
 		sprite.setPosition(x, y);
+		interactionWithMap();
+	}
+	
+	void interactionWithMap(){ //ф-ция взаимодействия с картой
+
+		for (int i = y / 25; i < (y + h) / 25; i++)//проходимся по всей карте, то есть по всем квадратикам размера 32*32, которые мы окрашивали в 9 уроке. про условия читайте ниже.
+			for (int j = x / 25; j<(x + w) / 25; j++){//икс делим на 32, тем самым получаем левый квадратик, с которым персонаж соприкасается. (он ведь больше размера 32*32, поэтому может одновременно стоять на нескольких квадратах). А j<(x + w) / 32 - условие ограничения координат по иксу. то есть координата самого правого квадрата, который соприкасается с персонажем. таким образом идем в цикле слева направо по иксу, проходя по от левого квадрата (соприкасающегося с героем), до правого квадрата (соприкасающегося с героем)
+				if ((TileMap[i][j] == '0') || (TileMap[i][j] == '1') || (TileMap[i][j] == '2'))//если наш квадратик соответствует символу 0 (стена), то проверяем "направление скорости" персонажа:
+				{
+					if (dy>0)//если мы шли вниз,
+					{
+						y = i * 25 - h;//то стопорим координату игрек персонажа. сначала получаем координату нашего квадратика на карте(стены) и затем вычитаем из высоты спрайта персонажа.
+					}
+					if (dy<0)
+					{
+						y = float(i) * 25.0f + 25.0f;//аналогично с ходьбой вверх. dy<0, значит мы идем вверх (вспоминаем координаты паинта)
+					}
+					if (dx>0)
+					{
+						x = j * 25 - w;//если идем вправо, то координата Х равна стена (символ 0) минус ширина персонажа
+					}
+					if (dx < 0)
+					{
+						x = j * 25 + 25;//аналогично идем влево
+					}
+				}
+
+				/*if (TileMap[i][j] == '3' || (TileMap[i][j] == '1') || (TileMap[i][j] == '2')) {
+					TileMap[i][j] = ' ';
+				}*/
+			}
 	}
 
 	/*float getplayercoordinateX() {
@@ -96,7 +100,7 @@ int main(){
 	s_map.setTexture(map);
 
 
-	Player p("hero.png", 134, 0, 47, 47);
+	Player p("hero.png", 300, 300, 37, 39);
 	while (window.isOpen()) {
 		float time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
 		clock.restart(); //перезагружает время
@@ -113,34 +117,36 @@ int main(){
 			p.dir = 1; p.speed = 0.1;
 			CurrentFrame += 0.005*time;
 			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(145, 45 * int(CurrentFrame), 47, 47));
+			p.sprite.setTextureRect(IntRect(145, 52 * int(CurrentFrame), 40, 40));
 			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Right)) {
 			p.dir = 0; p.speed = 0.1;
 			CurrentFrame += 0.005*time;
 			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(0, 45 * int(CurrentFrame), 47, 47));
+			p.sprite.setTextureRect(IntRect(0, 52 * int(CurrentFrame), 40, 40));
 			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Up)) {
 			p.dir = 3; p.speed = 0.1;
 			CurrentFrame += 0.005*time;
 			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(100, 45 * int(CurrentFrame), 47, 47));
+			p.sprite.setTextureRect(IntRect(100, 52 * int(CurrentFrame), 40, 40));
 			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			p.dir = 2; p.speed = 0.1;
 			CurrentFrame += 0.005*time;
 			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(50, 45 * int(CurrentFrame), 47, 47));
+			p.sprite.setTextureRect(IntRect(50, 52 * int(CurrentFrame), 40, 40));
 			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
 		}
 		p.update(time);
 		viewMap(time);
 		window.setView(view);
 		window.clear();
+
+		window.draw(p.sprite);
 
 		for (int i = 0; i < HEIGHT_MAP; i++)
 		for (int j = 0; j < WIDTH_MAP; j++) {
@@ -155,7 +161,6 @@ int main(){
 			s_map.setPosition(j * 25, i * 25);
 			window.draw(s_map);
 		}
-		window.draw(p.sprite);
 		window.display();
 	}
 
