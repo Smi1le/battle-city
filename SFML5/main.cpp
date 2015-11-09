bb213117a0a3ec2f84ba3a14fcbb7ff877f6e2d3
tima55510 <tima55510@gmail.com>
2015-11-10 00:10:26 +0300

    Реализована создание карты с помощью Tiledmap и её внедрение в игру. Так же добавлена обработка коллизий .

--- SFML5/main.cpp
+++ SFML5/main.cpp
@@ -1,89 +1,156 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include "level_1.h"
+#include <sstream>
+//#pragma comment (lib, "tinyxml.lib")
+//#include "level_1.h"
 #include "coordinate.h"
+#include "level.h"
+#include <vector>
+#include <list>
+//#include "TinyXML/tinyxml.h"
+//#include "TinyXML/tinyxml.cpp"
 
 ; using namespace sf;
 using namespace std;
 
-
-
-
-class Player {
-private:float x, y = 0;
+class Entity {
 public:
-	float w, h, dx, dy, speed = 0;
-	int dir;
-	int playerScore, health;
-	bool life;
-	String File;
-	Image image;
+	std::vector<Object> obj;//вектор объектов карты
+	float dx, dy, x, y, speed, moveTimer;
+	int w, h, health;
+	bool life, isMove, onGround;
 	Texture texture;
 	Sprite sprite;
-	Player(String F, float X, float Y, float W, float H) {
-		dir = 0; speed = 0; playerScore = 0; health = 100;
-		life = true;
-		File = F;
-		w = W; h = H;
-		image.loadFromFile("images/" + File);
-		image.createMaskFromColor(Color(0, 0, 0));
+	String name;
+	Entity(Image &image, String Name, float X, float Y, int W, int H) {
+		x = X; y = Y; w = W; h = H; name = Name; moveTimer = 0;
+		speed = 0; health = 100; dx = 0; dy = 0;
+		life = true; onGround = false; isMove = false;
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
-		x = X, y = Y;
-		sprite.setTextureRect(IntRect(0, 0, w, h));
 	}
 
-	void update(float time) {
-		switch (dir) {
-		case 0:dx = speed; dy = 0; break;
-		case 1:dx = -speed; dy = 0; break;
-		case 2:dx = 0; dy = speed; break;
-		case 3:dx = 0; dy = -speed; break;
+	FloatRect getRect() {//ф-ция получения прямоугольника. его коорд,размеры (шир,высот).
+		return FloatRect(x, y, w, h);//эта ф-ция нужна для проверки столкновений 
+	}
+};
+
+class Player: public Entity {
+public:
+	enum { left, right, up, down, jump, stay } state;
+	int playerScore;
+	bool cheats = false;
+	Player(Image &image, String name,Level &lev, float X, float Y, float W, float H):Entity(image,name,  X, Y, W, H) {
+		obj = lev.GetAllObjects();
+		playerScore = 0; state = stay;
+		if (name == "Player") {
+			sprite.setTextureRect(IntRect(0, 0, w, h));
 		}
-		x += dx*time;
-		y += dy*time;
-		speed = 0;
-		sprite.setPosition(x, y);
-		interactionWithMap();
 	}
-	
-	void interactionWithMap(){ //ф-ция взаимодействия с картой
 
-		for (int i = y / 25; i < (y + h) / 25; i++)//проходимся по всей карте, то есть по всем квадратикам размера 32*32, которые мы окрашивали в 9 уроке. про условия читайте ниже.
-			for (int j = x / 25; j<(x + w) / 25; j++){//икс делим на 32, тем самым получаем левый квадратик, с которым персонаж соприкасается. (он ведь больше размера 32*32, поэтому может одновременно стоять на нескольких квадратах). А j<(x + w) / 32 - условие ограничения координат по иксу. то есть координата самого правого квадрата, который соприкасается с персонажем. таким образом идем в цикле слева направо по иксу, проходя по от левого квадрата (соприкасающегося с героем), до правого квадрата (соприкасающегося с героем)
-				if ((TileMap[i][j] == '0') || (TileMap[i][j] == '1') || (TileMap[i][j] == '2'))//если наш квадратик соответствует символу 0 (стена), то проверяем "направление скорости" персонажа:
+	void control(float &CurrentFrame, float &time) {
+		if (Keyboard::isKeyPressed) {//если нажата клавиша
+			if (Keyboard::isKeyPressed(Keyboard::Left)) {//а именно левая
+				state = left; speed = 0.1;
+				CurrentFrame += 0.005*time;
+				if (CurrentFrame > 2) (CurrentFrame -= 2);
+				sprite.setTextureRect(IntRect(145, 52 * int(CurrentFrame), 40, 40));
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Right)) {
+				state = right; speed = 0.1;
+				CurrentFrame += 0.005*time;
+				if (CurrentFrame > 2) (CurrentFrame -= 2);
+				sprite.setTextureRect(IntRect(0, 52 * int(CurrentFrame), 40, 40));
+			}
+
+			if ((Keyboard::isKeyPressed(Keyboard::Up))) {//если нажата клавиша вверх и мы на земле, то можем прыгать
+				state = up; speed = 0.1;// onGround = false;//увеличил высоту прыжка
+				CurrentFrame += 0.005*time;
+				if (CurrentFrame > 2) (CurrentFrame -= 2);
+				sprite.setTextureRect(IntRect(100, 52 * int(CurrentFrame), 40, 40));
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Down)) {
+				state = down; speed = 0.1;
+				CurrentFrame += 0.005*time;
+				if (CurrentFrame > 2) (CurrentFrame -= 2);
+				sprite.setTextureRect(IntRect(50, 52 * int(CurrentFrame), 40, 40));
+			}
+		}
+	}
+
+	void checkCollisionWithMap(float Dx, float Dy)//ф ция проверки столкновений с картой
+	{
+		for (int i = 0; i<obj.size(); i++)//проходимся по объектам
+			if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+			{
+				if ((obj[i].name == "solid") || (obj[i].name == "easyEnemy"))//если встретили препятствие
 				{
-					if (dy>0)//если мы шли вниз,
-					{
-						y = i * 25 - h;//то стопорим координату игрек персонажа. сначала получаем координату нашего квадратика на карте(стены) и затем вычитаем из высоты спрайта персонажа.
-					}
-					if (dy<0)
-					{
-						y = float(i) * 25.0f + 25.0f;//аналогично с ходьбой вверх. dy<0, значит мы идем вверх (вспоминаем координаты паинта)
-					}
-					if (dx>0)
-					{
-						x = j * 25 - w;//если идем вправо, то координата Х равна стена (символ 0) минус ширина персонажа
-					}
-					if (dx < 0)
-					{
-						x = j * 25 + 25;//аналогично идем влево
-					}
+					if (Dy > 0) { y = obj[i].rect.top - h;  dy = 0;  onGround = true;}
+					if (Dy<0) { y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+					if (Dx>0) { x = obj[i].rect.left - w; }
+					if (Dx<0) { x = obj[i].rect.left + obj[i].rect.width; }
 				}
-
-				/*if (TileMap[i][j] == '3' || (TileMap[i][j] == '1') || (TileMap[i][j] == '2')) {
-					TileMap[i][j] = ' ';
-				}*/
 			}
 	}
 
-	/*float getplayercoordinateX() {
-		return x;
+	void update(float time, float &CurrentFrame) {
+		control(CurrentFrame, time);
+		switch (state) {
+		case right:dx = speed; dy = 0; break;
+		case left:dx = -speed; dy = 0; break;
+		case up:dx = 0; dy = -speed; break;
+		case down:dx = 0; dy = speed; break;
+		case stay: break;
+		}
+		x += dx*time;
+		checkCollisionWithMap(dx, 0);
+		y += dy*time;
+		checkCollisionWithMap(0, dy);
+		speed = 0;
+		sprite.setPosition(x, y);
 	}
-	float getplayercoordinateY() {
-		return y;
-	}*/
+};
 
+class Enemy :public Entity {
+public:
+	Enemy(Image &image, String Name, Level &lvl, float X, float Y, int W, int H) :Entity(image, Name, X, Y, W, H) {
+		obj = lvl.GetObjects("solid");//инициализируем.получаем нужные объекты для взаимодействия врага с картой
+		if (name == "EasyEnemy") {
+			sprite.setTextureRect(IntRect(10, 10, w, h));
+			//dx = 0.1;
+		}
+	}
+
+	void checkCollisionWithMap(float Dx, float Dy)
+	{
+		for (int i = 0; i<obj.size(); i++)//проходимся по объектам
+			if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+			{
+				//if (obj[i].name == "solid"){//если встретили препятствие (объект с именем solid)
+				if (Dy>0) { y = obj[i].rect.top - h;  dy = 0; onGround = true; }
+				if (Dy<0) { y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+				if (Dx>0) { x = obj[i].rect.left - w;  dx = -0.1; sprite.scale(-1, 1); }
+				if (Dx<0) { x = obj[i].rect.left + obj[i].rect.width; dx = 0.1; sprite.scale(-1, 1); }
+				//}
+			}
+	}
+
+	void update(float time)
+	{
+		if (name == "EasyEnemy") {
+			//moveTimer += time;if (moveTimer>3000){ dx *= -1; moveTimer = 0; }//меняет направление примерно каждые 3 сек
+			x += dx*time;
+			checkCollisionWithMap(dx, 0);
+			cout << "x = " << x << endl;
+			cout << "w = " << w << endl;
+			cout << "y = " << y << endl;
+			cout << "h = " << h << endl;
+			sprite.setPosition(x, y);
+			//sprite.setOrigin(0, 0);
+			//if (health <= 0) { life = false; }
+		}
+	}
 };
 
 int main(){
@@ -91,16 +158,27 @@ int main(){
 	view.reset(sf::FloatRect(0, 0, 800, 700));
 	Clock clock;
 	float CurrentFrame = 0;
+
+	Level lvl;//создали экземпляр класса уровень
+	lvl.LoadFromFile("map.tmx");//загрузили в него карту, внутри класса с помощью методов он ее обработает.
+
+	Image easyEnemyImage;
+	easyEnemyImage.loadFromFile("images/easyEnemy.png");
+	easyEnemyImage.createMaskFromColor(Color(255, 0, 0));
+
 	
-	Image map_image;
-	map_image.loadFromFile("images/Sprite's.png");
-	Texture map;
-	map.loadFromImage(map_image);
-	Sprite s_map;
-	s_map.setTexture(map);
+	Image hero;
+	hero.loadFromFile("images/hero.png");
+	hero.createMaskFromColor(Color::Black);
 
-
-	Player p("hero.png", 300, 300, 37, 39);
+	
+	Object player = lvl.GetObject("player");//объект игрока на нашей карте.задаем координаты игроку в начале при помощи него
+	Object easyEnemyObject = lvl.GetObject("easyEnemy");
+	
+	Player p(hero, "Player", lvl, player.rect.left, player.rect.top, 37, 39);//передаем координаты прямоугольника player из карты в координаты нашего игрока
+	Enemy easyEnemy(easyEnemyImage, "EasyEnemy", lvl, easyEnemyObject.rect.left, easyEnemyObject.rect.top, 37, 39); 
+	
+	//Player p( hero,"Player",lvl, 300, 300, 37, 39 );
 	while (window.isOpen()) {
 		float time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
 		clock.restart(); //перезагружает время
@@ -110,49 +188,26 @@ int main(){
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		//movement_hero(herosprite, time, CurrentFrame);
-		//std::cout << time << "\n";
-		//drawing(window, herosprite, map_s);
-		if (Keyboard::isKeyPressed(Keyboard::Left)) {
-			p.dir = 1; p.speed = 0.1;
-			CurrentFrame += 0.005*time;
-			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(145, 52 * int(CurrentFrame), 40, 40));
-			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+		
+		if (Keyboard::isKeyPressed(Keyboard::F12)) {
+			p.cheats = true;
 		}
-		else if (Keyboard::isKeyPressed(Keyboard::Right)) {
-			p.dir = 0; p.speed = 0.1;
-			CurrentFrame += 0.005*time;
-			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(0, 52 * int(CurrentFrame), 40, 40));
-			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+		if (Keyboard::isKeyPressed(Keyboard::F11)) {
+			p.cheats = false;
 		}
-		else if (Keyboard::isKeyPressed(Keyboard::Up)) {
-			p.dir = 3; p.speed = 0.1;
-			CurrentFrame += 0.005*time;
-			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(100, 52 * int(CurrentFrame), 40, 40));
-			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
-			p.dir = 2; p.speed = 0.1;
-			CurrentFrame += 0.005*time;
-			if (CurrentFrame > 2) (CurrentFrame -= 2);
-			p.sprite.setTextureRect(IntRect(50, 52 * int(CurrentFrame), 40, 40));
-			//getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
-		}
-		p.update(time);
-		viewMap(time);
+
+		p.update(time, CurrentFrame);
+		easyEnemy.update(time);
 		window.setView(view);
 		window.clear();
 
+		
+	
 		window.draw(p.sprite);
-
-		for (int i = 0; i < HEIGHT_MAP; i++)
+		lvl.Draw(window);
+		//window.draw(easyEnemy.sprite);
+		/*for (int i = 0; i < HEIGHT_MAP; i++)
 		for (int j = 0; j < WIDTH_MAP; j++) {
-			/*cout << "i = " << i << endl;
-			cout << "j = " << j << endl;
-			cout << "TileMap[i][j] = " << TileMap[i][j] << endl;*/
 			if (TileMap[i][j] == '0') (s_map.setTextureRect(IntRect(174, 0, 25, 25)));
 			if (TileMap[i][j] == '1') (s_map.setTextureRect(IntRect(0, 0, 25, 25)));
 			if (TileMap[i][j] == '2') (s_map.setTextureRect(IntRect(29, 0, 25, 25)));
@@ -160,7 +215,7 @@ int main(){
 			if (TileMap[i][j] == ' ') (s_map.setTextureRect(IntRect(145, 29, 25, 25)));
 			s_map.setPosition(j * 25, i * 25);
 			window.draw(s_map);
-		}
+		}*/
 		window.display();
 	}
 
